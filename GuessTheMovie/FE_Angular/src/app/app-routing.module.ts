e7810d1832f4c365164047cfff709a242c7f5cfd
Riccardo Pilloni <riccardopilloni84@gmail.com>
2022-05-10 07:04:34 +0200

    new structure

--- GuessTheMovie/FE_Angular/src/app/app-routing.module.ts
+++ GuessTheMovie/FE_Angular/src/app/app-routing.module.ts
@@ -1,19 +1,19 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router'
-import { CommentiComponent } from './components/create-comment/create-comment.component';
-import { DeleteCommentComponent } from './components/delete-comment/delete-comment.component';
-import { ListCommentsComponent } from './components/get-comment/get-comment.component';
+import { CommentiComponent } from './components/comment/create-comment/create-comment.component';
+import { DeleteCommentComponent } from './components/comment/delete-comment/delete-comment.component';
+import { ListCommentsComponent } from './components/comment/get-comment/get-comment.component';
 import { LoginComponent } from './components/login/login.component';
-import { GetRatingComponent } from './components/get-rating/get-rating.component';
-import { GetFavouriteMoviesComponent } from './components/get-favourite-movies/get-favourite-movies.component';
-import { CreateFavouriteMovieComponent } from './components/create-favourite-movie/create-favourite-movie.component';
-import { DeleteFavouriteMovieComponent } from './components/delete-favourite-movie/delete-favourite-movie.component';
-import { SignupComponent } from './components/register/signup.component';
+import { GetRatingComponent } from './components/rating/get-rating/get-rating.component';
+import { GetFavouriteMoviesComponent } from './components/favourites/get-favourite-movies/get-favourite-movies.component';
+import { CreateFavouriteMovieComponent } from './components/favourites/create-favourite-movie/create-favourite-movie.component';
+import { DeleteFavouriteMovieComponent } from './components/favourites/delete-favourite-movie/delete-favourite-movie.component';
+import { SignupComponent } from './components/login/register/signup.component';
 import { GetMovieDetailComponent } from './components/get-movie-detail/get-movie-detail.component';
 import { GameComponent } from './components/game/game.component';
-import { InsertRatingComponent } from './components/insert-rating/insert-rating.component';
+import { InsertRatingComponent } from './components/rating/insert-rating/insert-rating.component';
 import { HomeComponent } from './components/home/home.component';
-import { LogoutComponent } from './components/logout/logout.component';
+import { LogoutComponent } from './components/login/logout/logout.component';
 
 
 const routes: Routes = [
